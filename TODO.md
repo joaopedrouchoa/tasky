bbd30ddabe47bbd916d4e8c7f0827acabd835988
João Pedro Uchoa <joaopedropais432@gmail.com>
2023-07-13 21:37:33 -0300

    feat: created header, content, footer and task list

---
+++ TODO.md
@@ -0,0 +1,10 @@
+## Atividades
+- [x] Criar o cabeçalho da página com uma logo na esquerda e um botão de "adicionar tarefa" na esquerda.
+- [x] Criar o rodapé da página com informações da aplicação.
+- [x] Criar uma função no Javascript para carregar as tarefas.
+- [] Criar uma função no Javascript para adicionar uma nova tarefa.
+- [] Criar uma janela de confirmação para perguntar ao usuário se ele realmente deseja remover a tarefa.
+- [] Criar uma função no Javascript remover uma tarefa.
+- [] Criar uma janela pop-up com um formulário para possibilitar a edição dos dados da tarefa.
+- [] Criar uma função no Javascript para editar os dados de uma tarefa.
+- Criar uma função que retorne a data atual para configurar o ano no rodapé da página.
--- scripts/tasks.js
+++ scripts/tasks.js
@@ -7,7 +7,42 @@
 
 const tasks = [];
 
-function createTask(e) {  
+function loadTasks() {
+  const tasksMock = [
+    {
+      id: 1,
+      title: "Task 1",
+      description: "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Todo mundo vê os porris que eu tomo."
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      description: "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Todo mundo vê os porris que eu tomo."
+    },
+    {
+      id: 3,
+      title: "Task 3",
+      description: "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Todo mundo vê os porris que eu tomo."
+    },
+    {
+      id: 4,
+      title: "Task 4",
+      description: "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Todo mundo vê os porris que eu tomo."
+    },
+  ];
+
+  for(const task of tasksMock) {
+    let taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+    taskDiv.innerHTML = `<h2 class="task-title">${task.title}</h2><p class="task-description">${task.description}</p>`;
+
+    const tasksSection = document.getElementById("tasks");
+
+    tasksSection.appendChild(taskDiv);
+  };
+};
+
+function createTask() {  
   const title = document.getElementById("title");
   const description = document.getElementById("description");
 
@@ -26,4 +61,8 @@ function createTask(e) {
   taskContainer.appendChild(li);
 };
 
-function removeTask() {};
+function removeTask() {
+
+};
+
+loadTasks();
